88fea4d53f943e668ac509ff360983e1f5d1d78f
DotNet-FSE-ID-6404004 <aajy1677@gmail.com>
2025-06-28 21:31:09 +0530

    Add files via upload
    Signed-off-by: DotNet-FSE-ID-6404004 <aajy1677@gmail.com>

---
+++ week2/SQL/4.  Stored procedure - Exercise 5/5.return stored procedure from secific department.sql
@@ -0,0 +1,19 @@
+﻿
+
+
+create PROCEDURE spEmployee
+    @DepartmentID INT
+AS
+BEGIN
+    SELECT COUNT(*) AS TotalEmployees
+    FROM Employees
+    WHERE DepartmentID = @DepartmentID;
+END;
+GO
+
+EXEC spEmployee @DepartmentID = 2;
+GO
+select*from employees
+GO 
+select*from Departments
+GO
